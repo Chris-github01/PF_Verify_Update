56314ccf27c9ab9f06ed44a49769c696ee379305
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2025-11-30 11:22:32 +1300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,7 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        gray: {
-          50: '#f8fafc',
-          100: '#f1f5f9',
-          200: '#e2e8f0',
-          300: '#cbd5e1',
-          400: '#94a3b8',
-          500: '#64748b',
-          600: '#cbd5e1',
-          700: '#e2e8f0',
-          800: '#f1f5f9',
-          900: '#f8fafc',
-          950: '#020617',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
