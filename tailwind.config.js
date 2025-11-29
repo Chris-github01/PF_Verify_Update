1d9f91c918624dd9045c0b4afbab0871331ee9c9
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2025-11-30 11:19:50 +1300

    Updated index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#cbd5e1',
+          700: '#e2e8f0',
+          800: '#f1f5f9',
+          900: '#f8fafc',
+          950: '#020617',
+        },
+      },
+    },
   },
   plugins: [],
 }
