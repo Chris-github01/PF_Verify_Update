b096e355655a2a925d54d8caf4d832d6afe6722b
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2025-12-18 05:04:52 +1300

    Updated index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router';
+import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <Router />
+      <App />
     </ErrorBoundary>
   </React.StrictMode>
 );
