56e471b0117f0f8755f7e3e3b387589ea5f8d8d6
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2026-04-10 19:02:38 +1200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    hmr: {
-      timeout: 30000,
-    },
-  },
-});
+  plugins: [react(), tailwindcss()],
+})
